56859302393fdf62aed39bfe3744da5b0420edaa
Muhammad Obaidullah Ather <muhammad.obaidullah@objectsynergy.com>
2017-09-08 14:53:16 +0500

    init

---
+++ GL/GL.Tests/SimpleCalculationsTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GL;
+
+namespace GL.Tests
+{
+    [TestClass]
+    public class SimpleCalculationsTest
+    {
+        [TestMethod]
+        public void UserCanAddTwoNumbers()
+        {
+            Assert.AreEqual(4, Program.Add(2, 2), "Addition of two numbers failed");
+        }
+
+        [TestMethod]
+        public void UserCanSubtracTwoNumbers()
+        {
+            Assert.AreEqual(1, Program.Subtract(5, 4), "Subtraction of two numbers failed");
+        }
+
+        [TestMethod]
+        public void UserCanMultiplyTwoNumbers()
+        {
+            Assert.AreEqual(6, Program.Multiply(3, 2), "Multiplication of two numbers failed");
+        }
+    }
+}
---
+++ GL/GL/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GL
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Add(2, 3));
+        }
+
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+
+        public static int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+    }
+}
